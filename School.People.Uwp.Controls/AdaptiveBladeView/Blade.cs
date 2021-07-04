d4d855b1d8a5114577d82b1ff4de65aac4db1481
Jake Parane <jakes_parane@outlook.com>
2021-07-05 07:44:24 +0800

    ui controls update

--- School.People.Uwp.Controls/AdaptiveBladeView/Blade.cs
+++ School.People.Uwp.Controls/AdaptiveBladeView/Blade.cs
@@ -5,37 +5,8 @@ using Windows.UI.Xaml.Controls;
 
 namespace School.People.Uwp.Controls
 {
-    [DefaultProperty(nameof(Content))]
-    public class Blade : Control
+    public class Blade : ContentControl
     {
-        /// <summary>
-        /// Identifies the <see cref="ContentTemplate"/> property.
-        /// </summary>
-        private static readonly DependencyProperty ContentTemplateProperty = DependencyProperty.Register(nameof(ContentTemplate), typeof(DataTemplate), typeof(Blade), new PropertyMetadata(null));
-
-        /// <summary>
-        /// Gets/sets the <see cref="Blade"/>'s content template.
-        /// </summary>
-        public DataTemplate ContentTemplate
-        {
-            get { return (DataTemplate)GetValue(ContentTemplateProperty); }
-            set { SetValue(ContentTemplateProperty, value); }
-        }
-
-        /// <summary>
-        /// Identifies the <see cref="Content"/> property.
-        /// </summary>
-        private static readonly DependencyProperty ContentProperty = DependencyProperty.Register(nameof(Content), typeof(object), typeof(Blade), new PropertyMetadata(null));
-
-        /// <summary>
-        /// Gets/sets the <see cref="Blade"/>'s content.
-        /// </summary>
-        public object Content
-        {
-            get { return GetValue(ContentProperty); }
-            set { SetValue(ContentProperty, value); }
-        }
-
         public Blade() => DefaultStyleKey = typeof(Blade);
     }
 }
