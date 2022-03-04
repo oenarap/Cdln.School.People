b4a787dd56c1c17ec99a6ca9e15b8e6520310906
Jake Parane <jakes_parane@outlook.com>
2022-03-04 22:23:59 +0800

    fixed empty frame exeption

--- Cdln.School.People.Uwp/NavigationService.cs
+++ Cdln.School.People.Uwp/NavigationService.cs
@@ -9,17 +9,43 @@ namespace Cdln.School.People.Uwp
 {
     public sealed class NavigationService : INavigationService
     {
-        private readonly Frame frame;
+        private Frame _Frame;
+
+        private Frame Frame
+        {
+            get
+            {
+                if (_Frame != null) { return _Frame; }
+                if (_Frame == null)
+                {
+                    _Frame = Window.Current.Content as Frame;
+
+                    if (_Frame != null)
+                    {
+                        _Frame.NavigationFailed += OnNavigationFailed;
+                    }
+                }
+                return _Frame;
+            }
+            set
+            {
+                _Frame = value;
+
+                if (_Frame != null)
+                {
+                    _Frame.NavigationFailed += OnNavigationFailed;
+                }
+            }
+        }
         private NavigationContext context;
 
-        public bool CanGoBack => frame.CanGoBack;
+        public bool CanGoBack => Frame.CanGoBack;
 
-        public bool CanGoForward => frame.CanGoForward;
+        public bool CanGoForward => Frame.CanGoForward;
 
         public NavigationService(Frame initialFrame = null)
         {
-            frame = initialFrame ?? Window.Current.Content as Frame;
-            frame.NavigationFailed += OnNavigationFailed;
+            Frame = initialFrame;
         }
 
         private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
@@ -31,32 +57,32 @@ namespace Cdln.School.People.Uwp
         {
             if (CanGoBack)
             {
-                int lastIndex = frame.BackStackDepth - 1;
-                var entry = frame.BackStack[lastIndex];
-                frame.BackStack.RemoveAt(lastIndex);
+                int lastIndex = Frame.BackStackDepth - 1;
+                var entry = Frame.BackStack[lastIndex];
+                Frame.BackStack.RemoveAt(lastIndex);
                 var result = Navigate(entry.SourcePageType, context);
-                if (result == true) { frame.BackStack.RemoveAt(lastIndex); }
+                if (result == true) { Frame.BackStack.RemoveAt(lastIndex); }
                 return result;
             }
             return false;
         }
 
-        public void GoForward() => frame.GoForward();
+        public void GoForward() => Frame.GoForward();
 
         public bool Navigate(Type sourcePageType, NavigationContext context = null, NavigationTransitionInfo navigationTransitionInfo = null)
         {
-            var result = frame.Navigate(sourcePageType, context, navigationTransitionInfo);
+            var result = Frame.Navigate(sourcePageType, context, navigationTransitionInfo);
             if (result == true) { this.context = context; }
             return result;
         }
 
         public bool NavigateBack(Type sourcePageType, NavigationContext context = null, NavigationTransitionInfo navigationTransitionInfo = null)
         {
-            int count = frame.BackStackDepth;
+            int count = Frame.BackStackDepth;
             for (int i = count - 1; i >= 0; i--)
             {
-                var entry = frame.BackStack[i];
-                frame.BackStack.RemoveAt(i);
+                var entry = Frame.BackStack[i];
+                Frame.BackStack.RemoveAt(i);
                 if (entry.SourcePageType == sourcePageType) { return Navigate(sourcePageType, context, navigationTransitionInfo); }
             }
             return false;
